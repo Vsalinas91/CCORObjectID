fbbb961453bf7522e6936b3691c566cd0c9bbfad
Vsalinas91 <vicente.salinas@colorado.edu>
2025-08-11 14:28:25 -0600

    revise how data are stored per each input file iteration, and update call to write outputs per 15 window

--- object_identification/ccor_id.py
+++ object_identification/ccor_id.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from .utils.retrieve_data import (
     load_planetary_data,
     load_star_data,
@@ -15,9 +13,11 @@ from .utils.coordinate_transformations import (
     get_star_names,
     get_ccor_observer,
 )
+from .utils.exceptions import CCORExitError
 
 import os
 from skyfield.api import load
+from pathlib import Path
 
 
 def run_alg(inputs):
@@ -51,21 +51,22 @@ def run_alg(inputs):
     # Define the observer (approximate from GEO location for G19):
     observer = get_ccor_observer(earth)
 
-    # For now, just initialize empty lists - will look into a
-    # more effective solution later (maybe dict-like object)
-    star_dict = defaultdict(list)
-    planet_dict = defaultdict(list)
-    data_dict = defaultdict(list)
-    comet_dict = defaultdict(list)
-
     for f in inputs[:1]:  # 10]:
+        # For now, just initialize empty lists - will look into a
+        # more effective solution later (maybe dict-like object)
+        star_dict = {}
+        planet_dict = {}
+        data_dict = {}
+        comet_dict = {}
+
         print(f"Identifying objects for file: {os.path.basename(f)}")
         get_input_data = read_input(f, ts)
-        data = get_input_data.image_data
+        data = get_input_data.image_data  # noqa: F841
         wcs = get_input_data.WCS
         ccor_map = get_input_data.ccor_map
         t = get_input_data.time
         observation_time = get_input_data.obs_time
+        end_time = get_input_data.end_time
 
         # FOR STARS:
         # -----------
@@ -88,33 +89,32 @@ def run_alg(inputs):
         valid_pixels, get_comet, get_distance = get_comet_locations(comets, sun, ts, observer, t, wcs)
 
         # Append the data
-        comet_dict["comets"].append(get_comet)
-        comet_dict["comet_locs"].append(valid_pixels)
+        comet_dict["comets"] = get_comet
+        comet_dict["comet_locs"] = valid_pixels
 
         # Stars:
-        star_dict["stars"].append((good_sx_sub, good_sy_sub))
-        star_dict["star_markers"].append(good_markers_sub)
-        star_dict["star_ids"].append(good_star_ids)
-        star_dict["star_names"].append(good_star_names)
+        star_dict["stars"] = (good_sx_sub, good_sy_sub)
+        star_dict["star_markers"] = good_markers_sub.values.tolist()
+        star_dict["star_ids"] = good_star_ids.values.tolist()
+        star_dict["star_names"] = good_star_names
 
         # Planetary
         keys = ["mercury", "venus", "moon", "mars", "jupiter", "saturn", "uranus", "neptune"]
         for k in keys:
-            planet_dict[k].append(planet_locations[k])
+            planet_dict[k] = planet_locations[k]
 
         # Data:
-        data_dict["data"].append(data)
-        data_dict["date_obs"].append(observation_time)
+        data_dict["date_obs"] = observation_time
+        data_dict["date_end"] = end_time
+        data_dict["parent_filename"] = Path(f).name
 
-    # Now output the data:
-    write_output(
-        inputs,
-        planet_dict,
-        star_dict,
-        data_dict,
-        comet_dict,
-        get_star_names,
-    )
+        combined_dict = {**data_dict, **comet_dict, **planet_dict, **star_dict}
+
+        try:
+            write_output(observation_time, end_time, combined_dict)
+        except CCORExitError as e:
+            print(str(e))
+            return None
 
     # Plot if desired:
     # make_plots()
