6d87752159231cc9ea66a01b9fb7b18d70ff5f7b
Vsalinas91 <vicente.salinas@colorado.edu>
2025-08-25 13:27:03 -0600

    add script to run algorithm from cli

---
+++ object_identification/run_alg.py
@@ -0,0 +1,36 @@
+import glob
+import argparse
+import ccor_id
+
+
+def do_processing(
+    input_dir: str, generate_figures: bool = False, save_figures: bool = False, write_output_files: bool = False
+) -> None:
+    """
+    Run this script to execute the object identification using
+    data specified in the input_dir input argument on the command line.
+    """
+    # get files:
+    files = sorted(glob.glob(input_dir + "*.fits"))[:1]
+
+    # execute algorithm for input file stack:
+    # try:
+    ccor_id.run_alg(files, generate_figures, write_output_files, save_figures)
+    # except Exception:
+    #     print("Cannot process the current input file stack.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input_dir", type=str, required=True)
+    parser.add_argument("-f", "--gen_figures", type=bool, required=False)
+    parser.add_argument("-w", "--write_outputs", type=bool, required=False)
+    parser.add_argument("-s", "--save_figures", type=bool, required=False)
+
+    args = parser.parse_args()
+    input_dir = args.input_dir
+    generate_figures = args.gen_figures
+    save_figures = args.save_figures
+    write_outputs = args.write_outputs
+
+    do_processing(input_dir, generate_figures, write_outputs, save_figures)
