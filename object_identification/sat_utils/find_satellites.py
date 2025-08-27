4408458ce2c320bca87b19954fcb0f2011d3d85a
Vsalinas91 <vicente.salinas@colorado.edu>
2025-08-27 16:24:42 -0600

    add first round of satellite id utils

---
+++ object_identification/sat_utils/find_satellites.py
@@ -0,0 +1,385 @@
+import numpy as np
+from astropy.coordinates import SkyCoord, CartesianRepresentation
+import astropy.units as u
+from astropy.coordinates import TEME, CartesianDifferential, ITRS  # noqa: F401
+from sunpy.coordinates import frames  # noqa: F401
+
+from position_transformations import (
+    get_angle,
+    get_angular_positions,
+)
+
+
+##########################################################################
+# We Want the Positions for All possible Times During the Image Capture: #
+##########################################################################
+def get_all_positions_for_times(astro_times, j_times, earth, sun, ccor_map, valid_sat, use_gcrs=False):
+    """
+    Get the Sun, CCOR, Earth, and satellite positions for
+    all times reported in a CCOR data product file's header.
+
+    This ensures that we capture all possible satellites positions projected
+    to DATE-BEG=DATE-OBS, DATE-AVG, DATE-END.
+
+    The inputs are:
+
+        -) astro_times = astropy Time objects for date-obs, date-avg, date-end
+        -) j_times = jdate times for date-obs, date-avg, date-end for predicting positions
+        -) earth = earth ephemeris object
+        -) sun = sun ephemeris object
+        -) ccor_map = ccor_map object
+        -) valid_sat = all valid satellites (no duplicates)
+
+    There are several options for the coordinate transformations using toggle use_gcrs:
+
+        -) Use GCRS will cast all coordinates into the Geocentric Celestial Reference System
+        -) Not using GCRS will default the coordinates to being cast into a Geocentric Earth Equatorial System
+           which is more ideal when dealing with satellite positions relative to other celestial bodies.
+
+    Further, all coordinates are relative to the Earth's position, but this can be cast into a barycentric frame
+    by simply subtracting the Earth positions--at each time--from the Sun position, and removed from how th
+    satellite position vector is defined at the observation time.
+
+    Returns:
+        -) all_sat_names = all satellite names being checked
+        -) all_sat_coords = all satellite coordinates for all times
+        -) all_sun_coords = all sun coordinates for all times
+        -) all_ccor_coords = all ccor coordinates for all times
+        -) all_earth_coords = all earth coordinates for all times
+        -) all_sat_pos = all satellite positions at observation time, can be converted
+
+    """
+    # Init all lists
+    all_sat_names = []  # Satellite names
+    all_sat_coords = []  # satellite coordinates in GCRS/GEE cartesian
+    all_sun_coords = []  # Sun coordinates in GCRS/GEE cartesian
+    all_ccor_coords = []  # CCOR coordinates in GCRS/GEE cartesian
+    all_earth_coords = []  # Earth coordinates in GCRS/GEE cartesian
+    all_sat_pos = []  # Satellite position vector relative to Earth
+
+    # Iterate over the file times:
+    for at, t in zip(astro_times, j_times):
+
+        # Get Earth and Sun Locations at Observation Time
+        # ---------------------------------------------
+        earth_loc = earth.at(t)
+        sun_loc = sun.at(t)  # noqa: F841
+
+        # Observe the sun from the Earth to get it's position relative to Earth at the observatory time.
+        # and get it's position vector
+        s = earth_loc.observe(sun).apparent()
+        if use_gcrs:
+            # use GCRS
+            sunx, suny, sunz = s.position.km
+            earx, eary, earz = earth_loc.position.km
+        else:
+            # use GEE
+            srep = CartesianRepresentation(s.position.km * u.km)
+            scoord = SkyCoord(srep, frame="geocentricearthequatorial", representation_type="cartesian", obstime=at)
+
+            erep = CartesianRepresentation(earth_loc.position.km * u.km)
+            ecoord = SkyCoord(erep, frame="geocentricearthequatorial", representation_type="cartesian", obstime=at)
+
+            sunx, suny, sunz = scoord.cartesian.xyz.to(u.km).to_value()
+            earx, eary, earz = ecoord.cartesian.xyz.to(u.km).to_value()
+
+        # Get the CCOR Observer Coordinates:
+        # ------------------------------------------------
+        # Convert the CCOR observation location
+        if use_gcrs:
+            ccor_loc = ccor_map.observer_coordinate.transform_to("gcrs")
+        else:
+            ccor_loc = ccor_map.observer_coordinate.transform_to("geocentricearthequatorial")
+        # Make into a SkyCoord object
+        ccor_loc = SkyCoord(
+            CartesianRepresentation(
+                ccor_loc.cartesian.x.to(u.km), ccor_loc.cartesian.y.to(u.km), ccor_loc.cartesian.z.to(u.km)
+            ),
+            frame="geocentricearthequatorial",
+            representation_type="cartesian",
+            obstime=at,
+        )
+
+        # As with the earth satellite objects, we must also add the earth position to the CCOR position
+        # vector to account for a barycentric reference frame
+        # [DISABLE ADDING EARTH if not earth relative] for ccor AND sun vectors
+        ccor_x = ccor_loc.cartesian.x.to(u.km).to_value() + earx
+        ccor_y = ccor_loc.cartesian.y.to(u.km).to_value() + eary
+        ccor_z = ccor_loc.cartesian.z.to(u.km).to_value() + earz
+
+        # Commented out, but remove earth position if casting into barycentric frame
+        # sunx -= earx
+        # suny -= eary
+        # sunz -= earz
+
+        # Get satellite positions and ids
+        # --------------------------------------------------
+        sat_xyzs = []  # Satellite xyzs, same as coords
+        sat_name = []  # The satellite name, not NORAD Cat ID
+        sat_coords = []  # Satellite coordinates (actually the same thing)
+        sat_id = []  # satellite catalogue number
+        sat_pos = []  # satellite position at observation time
+        for i, sat in enumerate(valid_sat):
+            # Define the position relative to Earth in a barycentric reference frame
+            # ensures that all bodies are in the same reference frame
+            position = (earth + sat).at(t).position.km  # skip earth reference for now
+            # position = sat.at(t).position.km # remove earth if desired
+
+            # Get the satellites cartesian coordinates for transforming into a SkyCoord object
+            rep = CartesianRepresentation(position * u.km)
+            if use_gcrs:
+                # If using GCRS, insert frame here
+                sat_coord = SkyCoord(rep, frame="gcrs", obstime=at)
+            else:
+                # Else we use GEE
+                # p = (earth+sat).at(t).position.km
+                # v = (earth+sat).at(t).velocity.km_per_s
+                # teme_v = CartesianDifferential(v*u.km/u.s)
+                # teme_p = CartesianRepresentation(p*u.km)
+                # teme = TEME(teme_p.with_differentials(teme_v), obstime=at)
+
+                # rep = teme.transform_to(frames.GeocentricEarthEquatorial(obstime=at)).cartesian
+                sat_coord = SkyCoord(
+                    rep, frame="geocentricearthequatorial", obstime=at, representation_type="cartesian"
+                )
+
+            # Append all relevant satellite data
+            sat_coords.append(sat_coord)
+            sat_xyzs.append(sat_coord)
+            sat_name.append(sat.name)
+            sat_id.append(sat.model.satnum)
+            sat_pos.append((earth + sat).at(t))
+
+        # Define the CCOR To Sun Line-of-Sight (LOS):
+        # ---------------------------------------------------
+        # Define CCOR-to-Sun Line of Sight (LOS)
+        # Note: because it's CCOR relative, need to adjust angles by 180 if
+        # plotting the 3D satellite locations relative to CCOR's FOV
+        pos = np.sqrt((ccor_x - sunx) ** 2.0 + (ccor_y - suny) ** 2.0 + (ccor_z - sunz) ** 2.0)
+        az = np.arctan2((ccor_y - suny), (ccor_x - sunx))  # 180 - az in degrees if plotting grid
+        el = np.arccos((ccor_z - sunz) / pos)  # also needs to be adjusted if plotting  # noqa: F841
+        sc_angle = np.rad2deg(np.arccos((sunz - ccor_z) / pos))  # angle to sun's position
+        print(
+            f"Sun is at an inclination angle of {sc_angle} from "
+            + f"x-axis at time {t.utc_datetime()} and {az} from horizontal."
+        )
+
+        all_sat_names.append(sat_name)
+        all_sat_coords.append(sat_coords)
+        all_sun_coords.append((sunx, suny, sunz))
+        all_ccor_coords.append((ccor_x, ccor_y, ccor_z))
+        all_earth_coords.append((earx, eary, earz))
+        all_sat_pos.append(sat_pos)
+
+    # Prep the coordinate arrays for finding the closest positions by making them into arrays.
+    # For each Fits file all time stamps will be checked, so 3xN_satellites
+    all_sat_names = np.array(all_sat_names)
+    all_sat_coords = np.array(all_sat_coords)
+    all_sun_coords = np.array(all_sun_coords)
+    all_ccor_coords = np.array(all_ccor_coords)
+    all_earth_coords = np.array(all_earth_coords)
+    all_sat_pos = np.array(all_sat_pos)
+
+    # Find goes position:
+    goes_sat_coord = all_sat_coords[np.where(all_sat_names == "GOES 19")]
+
+    return (
+        all_sat_names,
+        all_sat_coords,
+        all_sun_coords,
+        all_ccor_coords,
+        all_earth_coords,
+        all_sat_pos,
+        goes_sat_coord,
+    )
+
+
+def get_satellites_in_fov(
+    tlabels,
+    all_ccor_coords,
+    all_sun_coords,
+    all_sat_coords,
+    all_sat_names,
+    all_sat_pos,
+    goes_sat_coord,
+    fov_angle=11,
+    radius_search=30e3,
+):
+    """
+    Identify all satellites in the CCOR FOV within 11 degrees of the boresight and
+    retrieve their cartesian locations, angular positions from the boresight, distances from
+    CCOR at their respective times:
+
+    Returns:
+       - get_angle_in_fov = the angle relative to the SUN-CCOR vector, boresight within the FOV
+       - get_dist = distnace from satellite to ccor within the FOV
+       - get_sat_id = name of satellites in FOV
+       - get_tlabel = get the time label for the position
+       - get_angle_locs = get the azimuthal and inclination angles for plotting
+       - get_sat_pos = get the satellite positions in the FOV
+       - get_sat_collation = get satellites locations within the search radius
+    """
+    # Initialize arrays of type object to store lists of varying sizes
+    get_angle_in_fov = np.zeros([len(tlabels)], dtype="object")
+    # get_pix_locs = np.zeros([len(tlabels)], dtype='object')
+    get_dist = np.zeros([len(tlabels)], dtype="object")
+    get_sat_id = np.zeros([len(tlabels)], dtype="object")
+    get_tlabel = np.zeros([len(tlabels)], dtype="object")
+    get_angle_locs = np.zeros([len(tlabels)], dtype="object")
+    get_sat_pos = np.zeros([len(tlabels)], dtype="object")
+    get_sat_collection = []
+
+    # Iterate over all 3 timestamps in CCOR file:
+    for tidx in np.arange(all_sat_names.shape[0]):
+        # Initialize lists to write to array objects for each time
+        get_close_points = []
+        get_close_ids = []
+        valid_angles = []
+        valid_locs = []  # noqa: F841
+        valid_ids = []
+        valid_tlabel = []
+        valid_dists = []
+        valid_angle_locs = []
+        valid_sat_pos = []
+
+        # Get the CCOR, SUN, and [optional] GOES19 positions
+        ccor_coord = all_ccor_coords[tidx]
+        sun_coord = all_sun_coords[tidx]
+        # g19_coord = goes_sat_coord[tidx].cartesian.xyz.to(u.km).to_value()
+
+        ccor_x = ccor_coord[0]
+        ccor_y = ccor_coord[1]
+        ccor_z = ccor_coord[2]
+
+        sunx = sun_coord[0]
+        suny = sun_coord[1]
+        sunz = sun_coord[2]
+
+        # g19x =  g19_coord[0]
+        # g19y =  g19_coord[1]
+        # g19z =  g19_coord[2]
+        # ccor_x = g19x
+        # ccor_y = g19y
+        # ccor_z = g19z
+
+        # Iterate over the satellites for each time
+        for id, ss, sat_pos in zip(all_sat_names[tidx], all_sat_coords[tidx], all_sat_pos[tidx]):
+            # Don't look at GOES-19 since CCOR is onboard GOES 19
+            if id == "GOES 19":
+                continue
+
+            # Get the satellite positions
+            s = ss.cartesian.xyz.to(u.km).to_value()
+            xrel = s[0]
+            yrel = s[1]
+            zrel = s[2]
+            # Search over valid radius box
+            if (
+                (np.abs(xrel - ccor_x) < radius_search)
+                & (np.abs(yrel - ccor_y) < radius_search)
+                & (np.abs(zrel - ccor_z) < radius_search)
+            ):
+                # Shouldn't be an issue, but if NOT GOES-19, then proceed
+                if "GOES 19" not in id:
+                    # Get the distance from ccor to the satellite
+                    spos = np.sqrt((ccor_x - xrel) ** 2.0 + (ccor_y - yrel) ** 2.0 + (ccor_z - zrel) ** 2.0)
+                    # Get distance from satellite to sun
+                    spos_srel = np.sqrt(  # noqa: F841
+                        ((xrel - ccor_x) - (sunx - ccor_x)) ** 2.0
+                        + ((yrel - ccor_y) - (suny - ccor_y)) ** 2.0
+                        + ((zrel - ccor_z) - (sunz - ccor_z)) ** 2.0
+                    )
+
+                    # Get the satellite angle relative to the sun center
+                    sat_angle = get_angle(
+                        np.array([xrel, yrel, zrel]), np.array([ccor_x, ccor_y, ccor_z]), np.array([sunx, suny, sunz])
+                    )
+
+                    # Get approximate Pixel locations: [no fully tested]
+                    # locs = get_ortho_angle(
+                    #     np.array([xrel, yrel, zrel]),
+                    #     np.array([ccor_x, ccor_y, ccor_z]),
+                    #     np.array([sunx, suny, sunz]),
+                    #     header['YAWFLIP']
+                    # )
+
+                    angular_locs = get_angular_positions(
+                        np.array([xrel, yrel, zrel]),
+                        np.array([ccor_x, ccor_y, ccor_z]),
+                        np.array([sunx, suny, sunz]),
+                    )
+
+                    # Get all satellite locations in the radius box
+                    get_close_points.append(((xrel - ccor_x), (yrel - ccor_y), (zrel - ccor_z)))
+                    get_close_ids.append(id)
+
+                    # Check if satellite is within boresight FOV:
+                    if (sat_angle) <= fov_angle / 2:
+                        # Correct for yaw flip:
+                        valid_angles.append(sat_angle)
+                        # valid_locs.append(locs)
+                        valid_ids.append(id)
+                        valid_dists.append(spos)
+                        valid_tlabel.append(tlabels[tidx])
+                        valid_angle_locs.append(angular_locs)
+                        valid_sat_pos.append(sat_pos)
+                        print(
+                            f"{id} has a SAT_ANGLE of {sat_angle} from the boresight | Angular Locations (x, y):"
+                            + f" {angular_locs} | Distance from CCOR is {spos} km."
+                        )
+
+        # Store in lists
+        get_angle_in_fov[tidx] = valid_angles
+        # get_pix_locs[tidx] = (valid_locs)
+        get_dist[tidx] = valid_dists
+        get_sat_id[tidx] = valid_ids
+        get_tlabel[tidx] = valid_tlabel
+        get_angle_locs[tidx] = valid_angle_locs
+        get_sat_collection.append(get_close_points)
+        get_sat_pos[tidx] = valid_sat_pos
+
+    return (get_angle_in_fov, get_dist, get_sat_id, get_tlabel, get_angle_locs, get_sat_collection, get_sat_pos)
+
+
+def create_cone_mask(shape, center, radius, height, angle, direction, grid):
+    """
+    Creates a 3D cone mask.
+
+    Args:
+    shape: Tuple (x, y, z) representing the dimensions of the 3D array.
+    center: Tuple (x, y, z) representing the center of the cone base.
+    radius: Radius of the cone base.
+    height: Height of the cone.
+    angle: Angle of the cone (in radians), where 0 is a straight line and pi/2 is a flat circle.
+    direction: Tuple (x, y, z) representing the direction of the cone's axis.
+
+    Returns:
+    A 3D NumPy array (boolean mask) representing the cone.
+    """
+
+    x, y, z = grid
+    x0, y0, z0 = center
+    dx, dy, dz = direction
+
+    # Normalize direction
+    norm = np.sqrt(dx**2 + dy**2 + dz**2)
+    dx /= norm
+    dy /= norm
+    dz /= norm
+
+    # Cone equation
+    # Distance along the axis
+    axis_dist = (x - x0) * dx + (y - y0) * dy + (z - z0) * dz
+
+    # Distance from the axis
+    perp_x = x - x0 - axis_dist * dx
+    perp_y = y - y0 - axis_dist * dy
+    perp_z = z - z0 - axis_dist * dz
+    perp_dist = np.sqrt(perp_x**2 + perp_y**2 + perp_z**2)
+
+    # Calculate cone radius at each point
+    cone_radius = (radius / height) * axis_dist
+
+    # Create mask
+    mask = (axis_dist >= 0) & (axis_dist <= height) & (perp_dist <= cone_radius)
+    return mask
---
+++ object_identification/sat_utils/position_transformations.py
@@ -0,0 +1,203 @@
+import numpy as np
+
+
+def get_angle(sat, ccor, sun):
+    """
+    Get the angle between two vectors (2d or 3d), this angle represents the angle
+    for two given vectors in 3D
+
+
+            |
+            |       SAT
+            |      x x x x x x SUN
+            |     x           x
+            |    x         x
+            |   x  O    x
+            |  x     x
+            | x   x
+            |x x
+            ------------------------
+
+    """
+    # Example points A, B, C in 3D
+    A = sun
+    B = ccor
+    C = sat
+
+    # Vectors from B to A and C
+    BA = A - B
+    BC = C - B
+
+    # Compute angle at B in radians
+    cos_theta = np.dot(BA, BC) / (np.linalg.norm(BA) * np.linalg.norm(BC))
+    theta_rad = np.arccos(cos_theta)
+
+    # Optionally convert to degrees
+    theta_deg = np.degrees(theta_rad)
+
+    # print(f"Angle at vertex B: {theta_rad:.4f} radians, or {theta_deg:.2f} degrees")
+    return theta_deg
+
+
+def get_angular_positions(sat, ccor, sun):
+    """
+    Assuming the satellite angle is a vector magnitude in
+    coordinates in degrees, find the x and y angular components
+    (i.e., azimuth and inclination).
+
+    To do this, we have to be creative: First, calculate the angle between the
+    CCOR-SUN FOV and the CCOR-SAT Locations in 2D, so the 2D Projection of the angles
+    (see get_angle) docstring.
+
+    Then, calculate the angle from the x-axis to the Sun and the sattelite, this will
+    help us determine if the satellite is to the right or left of the boresight
+
+            |
+            |       SAT
+            |      x           SUN      O=angle from x to SAT
+            |     x           x         A=angle from x to SUN
+            |    x         x
+            |   x  O    x               FACTOR=-1 if O>A (LEFT of boresight) else 1 if O<A (RIGHT of boresight)
+            |  x     x
+            | x   x
+            |x x      A
+            ------------------------
+
+
+    Then, using the 3D vector positions, calculate the satellite and sun elevation angles. The difference
+    between SAT elevation and Sun elevation will give us the approximate position of the
+    y-angular component that falls on the circle. This assumes that we project the elevation angles
+    onto a 2D vertical plane and the difference between the two angles gives us the approximate angle
+    between the CCOR-SUN line-of-sight vector and the satellite position.
+
+    """
+    # First, get the azimuthal angle relative to the x-y plane only
+    azim = get_angle(sat[:2], ccor[:2], sun[:2])
+
+    # Now, determine if the satellite is left or right of boresight
+    angle_to_sun_horizontal = np.rad2deg(np.arctan2((sun[1] - ccor[1]), (sun[0] - ccor[0])))
+    angle_to_sat_horizontal = np.rad2deg(np.arctan2((sat[1] - ccor[1]), (sat[0] - ccor[0])))
+    if angle_to_sun_horizontal < angle_to_sat_horizontal:
+        factor = -1
+    else:
+        factor = 1
+
+    # Redefine the vector magnitudes
+    spos = ((sat[0] - ccor[0]) ** 2 + (sat[1] - ccor[1]) ** 2 + (sat[2] - ccor[2]) ** 2) ** 0.5
+    pos = ((sun[0] - ccor[0]) ** 2 + (sun[1] - ccor[1]) ** 2 + (sun[2] - ccor[2]) ** 2) ** 0.5
+
+    # Calculate the vertical angle from the horizontal plane from the origin (ccor)
+    sat_el = np.rad2deg(np.arccos((sat[-1] - ccor[-1]) / spos))
+
+    # Now, calculate the vertical angle from ccor to the sun and subtract the sat vertical angle
+    # to get the angle between them
+    sun_ccor_angle = np.rad2deg(np.arccos((sun[-1] - ccor[-1]) / pos))
+    s_inclination = sat_el - sun_ccor_angle
+
+    return (factor * azim, s_inclination)
+
+
+def get_ortho_angle(sat, ccor, sun, yaw_status, ccor_fov=5.5 * 2):
+    """
+    Get approximate pixel locations of projected satellite vectors on CCOR Plane
+    """
+    # Example points A, B, C in 3D
+    A = sun
+    B = ccor
+    C = sat
+
+    # Vectors from B to A and C
+    F = A - B  # Sun-CCOR
+    V = C - B  # Sat-CCOR
+
+    # Define unit normal of the SUN-CCOR and SAT-CCOR Vectors
+    F_norm = F / np.linalg.norm(F)
+    V_norm = V / np.linalg.norm(V)
+
+    # Define World Up Vector along z axis
+    U_world = np.array([-1, -1, 1])  # use the [0,0,1] to get the location relative to our aperature
+    # U_world = rotate_up_vector(U_world, F_norm, np.deg2rad(header['CROTA']))
+
+    # Define the image radius
+    # For level 3 images, need to divide by 2
+    image_radius = 5.5 * 3600 / 19.30 / 2  # noqa: F841
+
+    # Define the "image" Right Vector or vector
+    # defining the "horizontal" axis
+    R = np.cross(U_world, F_norm)
+    if np.linalg.norm(R) < 1e-6:
+        print("update world")
+        U_world = np.array([0, 1, 0])
+        R = np.cross(U_world, F_norm)
+    R_norm = R / np.linalg.norm(R)
+
+    # Calculate the true up-vector that defines the vertical
+    # image axis:
+    U_img = np.cross(F_norm, R_norm)
+    U_norm = U_img / np.linalg.norm(U_img)
+
+    # Now, project satellite vector to image plane:
+    V_perp = V_norm - np.dot(np.dot(V_norm, F_norm), F_norm)
+    # V_perp = rotate_up_vector(U_world, F_norm, np.deg2rad(26))
+
+    # Get pix projections onto the orthonormal axes:
+    x = np.dot(V_perp, R_norm)
+    y = np.dot(V_perp, U_norm)
+    z = np.dot(V_perp, F_norm)  # noqa: F841
+
+    # Now, we want to define the scaling parameters for getting the
+    # x and y locations into units pixels
+    width = 1024  # 1024
+    height = 960  # 960
+
+    fov_hor = np.deg2rad(ccor_fov)
+    pix_per_radx = width / fov_hor
+
+    aspect_ratio = width / height  # noqa: F841
+    fov_vert = np.deg2rad(ccor_fov)  # 2 * np.arctan(np.tan(fov_hor / 2) * aspect_ratio)
+    pix_per_rady = height / fov_vert
+
+    pix_x = width / 2 + x * pix_per_radx  # use width/2 instead of header['CRPIX1'] if desired
+    pix_y = height / 2 - y * pix_per_rady
+
+    # new:
+    # pix_x = (x/z) * (width/2) / np.arctan(fov_hor/2) + width/2
+    # pix_y = -(y/z) * (height/2) / np.arctan(fov_hor/2) + height/2
+
+    if yaw_status > 0:
+        return (
+            width - pix_x,
+            height - pix_y,
+        )  # this will depend on the yaw state, so we'll need to add a yaw_flip arg and not subtract if yaw_flip is 0
+    else:
+        return (pix_x, pix_y)
+
+
+def rotate_point(point, center, angle_degrees):
+    """Rotates a point around a center point by a given angle.
+
+    Args:
+        point (tuple): The (x, y) coordinates of the point to rotate.
+        center (tuple): The (x, y) coordinates of the center of rotation.
+        angle_degrees (float): The angle of rotation in degrees.
+
+    Returns:
+        tuple: The (x, y) coordinates of the rotated point.
+    """
+    x, y = point
+    cx, cy = center
+    angle_radians = np.deg2rad(angle_degrees)
+
+    # Translate to origin
+    translated_x = x - cx
+    translated_y = y - cy
+
+    # Rotate
+    rotated_x = translated_x * np.cos(angle_radians) - translated_y * np.sin(angle_radians)
+    rotated_y = translated_x * np.sin(angle_radians) + translated_y * np.cos(angle_radians)
+
+    # Translate back
+    final_x = rotated_x + cx
+    final_y = rotated_y + cy
+
+    return final_x, final_y
