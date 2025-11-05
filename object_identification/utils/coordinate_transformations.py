37b9c82ff9bdc6e83af05752b4d245dec4d9b9ac
Vsalinas91 <vicente.salinas@colorado.edu>
2025-11-04 19:34:11 -0700

    dont scale comet pixel coordinates - was using bad ncei l3 data

--- object_identification/utils/coordinate_transformations.py
+++ object_identification/utils/coordinate_transformations.py
@@ -104,9 +104,7 @@ def get_comet_locations(
         comet_ra, comet_dec, distance = comet_position.radec()
         # Get the comet position
         comet_x, comet_y = wcs.all_world2pix(comet_ra.degrees, comet_dec.degrees, 1)  # 1 for origin at 1
-        if which == "all":
-            comet_x /= 2
-            comet_y /= 2
+
         # Make sure it's withing the FOV bounds:
         if (
             (comet_x <= image_shape[1])
